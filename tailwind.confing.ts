adb147ecc6dd205cf175696659390e8287d97678
Vladimir <zajtzeffvl4d@yandex.ru>
2025-05-12 20:52:29 +0300

    Layot and first commit

---
+++ tailwind.confing.ts
@@ -0,0 +1,10 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/shared/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/views/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/widgets/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+} satisfies Config;
