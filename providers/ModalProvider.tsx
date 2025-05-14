329e8b12f6dac0a0624f288a68ec2cb812220cda
Vladimir <zajtzeffvl4d@yandex.ru>
2025-05-14 19:13:59 +0300

    fix

--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react"
 import AuthModal from "@/components/AuthModal"
 import UploadModal from "@/components/UploadModal"
 import SubscribeModal from "@/components/SubscribeModal"
+import { ProductWithPrice } from "@/types"
 
 interface ModalProviderProps {
     products: ProductWithPrice[];
